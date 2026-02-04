c62b55009643321194416ed69d5d6c4011e82b98
Marcelo <marceloalarconbarrenechea@gmail.com>
2026-02-03 20:15:54 -0400

    feat(post) add check_locale method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,32 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  include Pundit::Authorization
+
+  before_action :authenticate_user!
+  before_action :check_locale
+
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  private
+
+  def check_locale
+    allowed_locales = %w[en es pt]
+    settings = cookies.signed[:_rails_genius_settings]
+
+    if settings.present? && settings.dig("locale").present? && allowed_locales.include?(settings["locale"])
+      I18n.locale = settings["locale"].to_sym
+    else
+      I18n.locale = :en
+
+      cookies.signed[:_rails_genius_settings] = {
+        value: { locale: "en" },
+        expires: 1.year.from_now,
+        secure: Rails.env.production?,
+        same_site: :strict,
+        httponly: false
+      }
+    end
+  end
 end
